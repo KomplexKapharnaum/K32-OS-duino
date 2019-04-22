24bb1691db9871f53511e7a75344c98ba2901305
maigre <thomas.bohl@gmail.com>
2019-04-22 16:01:06 +0200

    Audio and conf

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,29 @@
 K32* engine;
 
 void setup() {
-  engine = new K32();
-}
+  
+  engine = new K32({
+    .stm32    = false,     // stm32 event listening and battery monitoring
+    .leds     = true,     // dual ws2812 
+    .audio    = true,     // audio engine with PCM51xx sound card
+    .sampler  = true,     // media indexing to midi bank/note-xxx
+    .osc      = 1818,     // osc port (0 = disable)
+    .wifi     = {         // wifi settings ({} to disable)
+      .ssid = "interweb",             // ssid (NULL to disable)
+      .password = "superspeed37",     // password (NULL if not secured)
+      .ip = NULL                      // static ip (NULL to use DHCP)
+    }
+  });
 
-int testkey = 0;
+}
 
 void loop() {
 
   if (engine->stm32->dblclicked()) engine->stm32->reset();
 
 
-  // leds->play( K32_leds_anims::sinus );
-  // delay(4000);
-
-  // bool playing = audio->run();
-
   // AUDIO TEST
+  // static int testkey = 0;
   // if (!playing) {
   //   testkey = testkey%2 + 1;
   //   audio->volume(100);
