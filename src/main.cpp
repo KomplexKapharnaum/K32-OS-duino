d09c473fe960b1eef80a6b53ef105166b152abca
maigre <thomas.bohl@gmail.com>
2019-04-25 00:50:54 +0200

    Audio improvements

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,3 @@
-
-
-
 #define K32_VERSION  0.9   // Refactoring
 
 #include "K32.h"
@@ -8,25 +5,30 @@
 K32* engine;
 
 void setup() {
-  
+
   engine = new K32({
     .stm32    = false,     // stm32 event listening and battery monitoring
     .leds     = true,     // dual ws2812 
     .audio    = true,     // audio engine with PCM51xx sound card
     .sampler  = true,     // media indexing to midi bank/note-xxx
-    .osc      = 1818,     // osc port (0 = disable)
     .wifi     = {         // wifi settings ({} to disable)
       .ssid = "interweb",             // ssid (NULL to disable)
       .password = "superspeed37",     // password (NULL if not secured)
-      .ip = NULL                      // static ip (NULL to use DHCP)
+      .ip = NULL,                      // static ip (NULL to use DHCP)
+      .osc = 1818                       // osc port (0 = disable)
     }
   });
 
+  if (engine->audio) {
+    engine->audio->loop(true);
+    engine->audio->play( engine->sampler->path( 0, 1 ) );
+  }
 }
 
 void loop() {
 
   if (engine->stm32->dblclicked()) engine->stm32->reset();
+  delay(10);
 
 
   // AUDIO TEST
@@ -43,3 +45,11 @@ void loop() {
   //   osc_newChan(false);
   // }
 }
+
+/*
+NOTES TODO !!
+
+- CLICKS and BLIPS audio => underflow dma zero ?
+- LEDS: stop anim in progress (better time frame)
+
+*/
