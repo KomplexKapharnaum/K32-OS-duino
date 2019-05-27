18c6ff19844eccd8a2deb5914573aba082ae436d
Pistache <clem.gangneux@gmail.com>
2019-05-27 16:12:38 +0200

    Add Wifi signal strength visualisation on double click of button

--- src/main.cpp
+++ src/main.cpp
@@ -8,29 +8,29 @@ void setup() {
 
   engine = new K32({
     .stm32    = true,     // stm32 event listening and battery monitoring
-    .leds     = false,     // dual ws2812 
+    .leds     = false,     // dual ws2812
     .audio    = true,     // audio engine with PCM51xx sound card
     .sampler  = true,     // media indexing to midi bank/note-xxx
-    .wifi     = {         
-      .ssid = "interweb",             // ssid (NULL to disable)
-      .password = "superspeed37",     // password (NULL if not secured)
+    .wifi     = {
+      .ssid = "kxkm-wfi",             // ssid (NULL to disable)
+      .password = "KOMPLEXKAPHARNAUM",     // password (NULL if not secured)
       .ip = NULL                      // static ip (NULL to use DHCP)
     },
-    .osc  = {         
+    .osc  = {
       .port_in  = 1818,             // osc port input (0 = disable)
       .port_out = 1819,             // osc port output (0 = disable)
       .beatInterval     = 0,              // heartbeat interval milliseconds (0 = disable)
       .beaconInterval   = 500             // full beacon interval milliseconds (0 = disable)
     }
   });
-  
+
   // Settings SET
   #ifdef NODEID
     engine->settings->set("id", NODEID);
     engine->settings->set("channel", 15);
   #endif
     engine->settings->set("model", 2);   // 0: proto -- 1: big -- 2: small
-  
+
 
 }
 
@@ -48,25 +48,49 @@ void loop() {
   //     engine->leds->play("sinus");
   //   }
   // }
-  // else if (engine->stm32->dblclicked()) 
+  // else if (engine->stm32->dblclicked())
   //   engine->stm32->reset();
 
   if (engine->stm32->clicked()) {
-    if (engine->audio->isPlaying()) {
-      engine->audio->stop();
-    }
-    else {
-      engine->audio->loop(true);
-      engine->audio->play( "/test.mp3" );
-      engine->audio->volume(20);
-    }
+    // if (engine->audio->isPlaying()) {
+    //   engine->audio->stop();
+    // }
+    // else {
+    //   engine->audio->loop(true);
+    //   engine->audio->play( "/test.mp3" );
+    //   engine->audio->volume(20);
+    // }
+
   }
-  else if (engine->stm32->dblclicked()) 
-    engine->stm32->reset();
+  else if (engine->stm32->dblclicked())
+  {
+      uint8_t led_RSSI[6];
+      uint8_t led_off[6] = {0, 0, 0, 0, 4, 0};
+
+      engine->wifi->getWiFiLevel(led_RSSI);
+      engine->stm32->blink(led_RSSI, 1000);
+
+
+      // long startTime = millis();
+      // long currentTime = millis();
+      // while(millis()-startTime < 1200)
+      // {
+      //   if((millis()-currentTime)<=100)
+      //   {
+      //     engine->stm32->leds(led_RSSI);
+      //   } else if ((millis()-currentTime)<=200)
+      //   {
+      //     engine->stm32->leds(led_off);
+      //   } else
+      //   {
+      //       currentTime = millis();
+      //   }
+    //   }
+    // }
+  }
 
   delay(10);
 
   // LOGF("Heap: %d \n", xPortGetFreeHeapSize());
 
 }
-
