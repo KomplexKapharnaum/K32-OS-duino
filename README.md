f26eb46031cead3b1b0575b3cd47bb72b2cce5b4
maigre <thomas.bohl@gmail.com>
2019-07-11 17:18:33 +0200

    mon

--- README.md
+++ README.md
@@ -12,20 +12,41 @@ To start working with this project:
 - Build and Flash !
 
 
-TODO:
+CHALON:
 
+
+BUG:
 - STM32: btn bug without serial...
-
-- ARTNET: node
-- ARTNET: to/from DMX
-- OSC: Beacon: JSON
-- OSC: Burst (msg id)
-- OSC: LOG args on receive
-- Monitor system 
-- MIDI: Bridge (separated)
 - AUDIO: CLICKS and BLIPS audio
+
+
+MINOR:
+- ARTNET: to/from DMX
+- OSC: LOG args on receive
 - OSC: channel change
-- OSC: beat Multicast
-- Websocket ? MQTT ?
 - Unbind sd (init and check) from audio engine
+- SYS: hardware version (+pinout) from stm32
+- SYS: id from stm32 
+
+MAJOR:
+- ARTNET: node
+- MQTT: interface
+- OSC: Burst (msg id)
+- OSC: Beacon: JSON
+- MIDI: Standalone bridge + MQTT + OSC Burst
+- Monitor system 
+- Audio sleep: replace delay(10) with mutex
+
+
+FUTURE:
+- OSC: beat Multicast
 - Better conf system (with webpage ? and AP ?)
+
+
+MQTT:
+- BROKER raspi (mosquitto) // mDNS declaration
+- MIDI bridge: virtual midi OUT // discover (mDNS) + connect MQTT broker // publish on [midi/c16/noteon]
+- ESP MQTT client: discover (mDNS) + connect MQTT broker
+    - audio/midi: subscribe to [midi/c16/#] // bind to audio player
+    - monitoring: publish on [status/esp/32]
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-// #define NODEID 42
+// #define NODEID 32
 
 #include "K32.h"
 
@@ -17,42 +17,56 @@ void setup() {
       .ip = NULL                    // static ip (NULL to use DHCP)
     },
     .osc  = {
-      .port_in  = 1818,             // osc port input (0 = disable)
-      .port_out = 1819,             // osc port output (0 = disable)
+      .port_in  = 1818,             // osc port input (0 = disable)  // 1818
+      .port_out = 1819,             // osc port output (0 = disable) // 1819
       .beatInterval     = 0,        // heartbeat interval milliseconds (0 = disable)
-      .beaconInterval   = 5000      // full beacon interval milliseconds (0 = disable)
+      .beaconInterval   = 3000      // full beacon interval milliseconds (0 = disable)
+    },
+    .mqtt = {
+      .broker = "2.0.0.1",      //"2.0.0.1"
+      .beatInterval = 5000            // heartbeat interval milliseconds (0 = disable)
     }
+
   });
 
   // Settings SET
   #ifdef NODEID
     engine->settings->set("id", NODEID);
-    engine->settings->set("channel", 15);
   #endif
+    engine->settings->set("channel", 1);
     engine->settings->set("model", 2);   // 0: proto -- 1: big -- 2: small
 
 
 }
 
+#include "driver/i2s.h"
+
 void loop() {
 
-  // if (engine->stm32->clicked()) {
-  //   if (engine->audio->isPlaying() || engine->leds->isPlaying()) {
-  //     engine->audio->stop();
-  //     engine->leds->stop();
-  //   }
-  //   else {
-  //     engine->audio->loop(true);
-  //     engine->audio->play( "/test.mp3" );
-  //     engine->audio->volume(50);
-  //     engine->leds->play("sinus");
-  //   }
-  // }
-  // else if (engine->stm32->dblclicked())
-  //   engine->stm32->reset();
+  if (engine->stm32->clicked()) {
+    // if (engine->audio->isPlaying() || engine->leds->isPlaying()) {
+    //   engine->audio->stop();
+    //   // engine->leds->stop();
+    // }
+    // else {
+    //   engine->audio->loop(true);
+    //   engine->audio->play( engine->sampler->path(3, 0)  );
+    //   engine->audio->volume(50);
+    //   // engine->leds->play("sinus");
+    // }
+    engine->stm32->send(K32_stm32_api::SET_LOAD_SWITCH, 0);
+    delay(2000);
+    engine->stm32->send(K32_stm32_api::SET_LOAD_SWITCH, 1);
+  }
+  else if (engine->stm32->dblclicked())
+    engine->stm32->reset();
 
-  delay(10);
-
+  delay(2000);
   // LOGF("Heap: %d \n", xPortGetFreeHeapSize());
+
+  // LOGINL("AUDIO: Power state = ");
+  // LOG(engine->audio->pcm->getPowerState());
+  // LOGINL("AUDIO: Automute flag = ");
+  // LOG(engine->audio->pcm->readRegister(PCM51xx::AUTO_MUTE_FLAGS));
 
 }
