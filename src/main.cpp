dc5f0336423b564cbc99d1a7e714d21d21410ab7
Pistache <clem.gangneux@gmail.com>
2020-01-23 17:53:21 +0100

    Full Leds Gauge working with several scenarii

--- src/main.cpp
+++ src/main.cpp
@@ -8,147 +8,26 @@
 
 
 
-/*******************************************************************************
-* Function Name  : void base_draw(void)
-* Description    :
-* Input          : None
-* Output         : None
-* Return         : None
-*******************************************************************************/
-void base_draw(void)
-{
-  int i, j;
-
-  /*
-  draw pixel
-  */
-  epd_clear();
-  for (j = 0; j < 600; j += 50)
-  {
-    for (i = 0; i < 800; i += 50)
-    {
-      epd_draw_pixel(i, j);
-      epd_draw_pixel(i, j + 1);
-      epd_draw_pixel(i + 1, j);
-      epd_draw_pixel(i + 1, j + 1);
-    }
-  }
-  epd_udpate();
-  delay(3000);
-
-  /*
-  draw line
-  */
-  epd_clear();
-  for (i = 0; i < 800; i += 100)
-  {
-    epd_draw_line(0, 0, i, 599);
-    epd_draw_line(799, 0, i, 599);
-  }
-  epd_udpate();
-  delay(3000);
-
-  /*
-  fill rect
-  */
-  epd_clear();
-  epd_set_color(BLACK, WHITE);
-  epd_fill_rect(10, 10, 100, 100);
-
-  epd_set_color(DARK_GRAY, WHITE);
-  epd_fill_rect(110, 10, 200, 100);
-
-  epd_set_color(GRAY, WHITE);
-  epd_fill_rect(210, 10, 300, 100);
-
-  epd_udpate();
-  delay(3000);
-
-  /*
-  draw circle
-  */
-  epd_set_color(BLACK, WHITE);
-  epd_clear();
-  for (i = 0; i < 300; i += 40)
-  {
-    epd_draw_circle(399, 299, i);
-  }
-  epd_udpate();
-  delay(3000);
-
-  /*
-  fill circle
-  */
-  epd_clear();
-  for (j = 0; j < 6; j++)
-  {
-    for (i = 0; i < 8; i++)
-    {
-      epd_fill_circle(50 + i * 100, 50 + j * 100, 50);
-    }
-  }
-  epd_udpate();
-  delay(3000);
-
-  /*
-  draw triangle
-  */
-  epd_clear();
-  for (i = 1; i < 5; i++)
-  {
-    epd_draw_triangle(399, 249 - i * 50, 349 - i * 50, 349 + i * 50, 449 + i * 50, 349 + i * 50);
-  }
-  epd_udpate();
-  delay(3000);
-}
-
-void draw_text_demo(void)
-{
-  char buff[] = {'G', 'B', 'K', '3', '2', ':', ' ', 0xc4, 0xe3, 0xba, 0xc3, 0xca, 0xc0, 0xbd, 0xe7, 0};
-  epd_set_color(BLACK, WHITE);
-  epd_clear();
-  epd_set_ch_font(GBK32);
-  epd_set_en_font(ASCII32);
-  epd_disp_string(buff, 0, 50);
-  epd_disp_string("ASCII32: Hello, World!", 0, 300);
-
-  epd_set_ch_font(GBK48);
-  epd_set_en_font(ASCII48);
-  buff[3] = '4';
-  buff[4] = '8';
-  epd_disp_string(buff, 0, 100);
-  epd_disp_string("ASCII48: Hello, World!", 0, 350);
-
-  epd_set_ch_font(GBK64);
-  epd_set_en_font(ASCII64);
-  buff[3] = '6';
-  buff[4] = '4';
-  epd_disp_string(buff, 0, 160);
-  epd_disp_string("ASCII64: Hello, World!", 0, 450);
-
-
-  epd_udpate();
-  delay(3000);
-}
-
-void draw_bitmap_demo(void)
-{
-  epd_clear();
-  epd_disp_bitmap("i1.BMP", 0, 0);
-  epd_udpate();
-  delay(3000);
-
-  epd_clear();
-  epd_disp_bitmap("i2.BMP", 0, 0);
-  epd_udpate();
-}
-
-
-
 K32* engine;
 unsigned long currentTime=millis();
+unsigned long loopTime = millis();
 bool previousChargeState = true; // t
 K32_leds_anim* LedsAnim ;
+// Current sensor on GPIO 35
+const int CurrentSensor = 35;
+
+// variable for storing power value value
+int PowerValue = 0;
+int RefValue = 0;
+int mode = 0;
+bool sinusOn = true;
+const int TimeOut = 5; // TimeOut of Leds anims in secs.  0 = infinite
+const int InterruptTime = 10 ; // Time of Leds anim interrupt in secs
+
+const int Color1[4] = {0,100,0,0};
+const int Color2[4] = {100,75,0,0};
+const int Color3[4] = {100,0,0,0};
+
 
 
 void setup() {
@@ -156,7 +35,7 @@ void setup() {
   engine = new K32({
     .stm32    = true,     // stm32 event listening and battery monitoring
     .leds     = true,     // dual ws2812
-    .audio    = true,     // audio engine with PCM51xx sound card
+    .audio    = false,     // audio engine with PCM51xx sound card
     .sampler  = true,     // media indexing to midi bank/note-xxx
     .power    = true,    // power monitoring
     .wifi     = {
@@ -181,23 +60,16 @@ void setup() {
 
    if (engine->power)
     engine->power->start();
-   engine->audio->volume(10);
 
 
    // engine->audio->loop(true);
    // engine->audio->volume(5);
    // engine->audio->play( "/robot.mp3" );
-   // engine->leds->play("sinus");
-
-
-
-
-   //swSer.begin(19200);
-   //Serial1.begin(19200, SERIAL_8N1, 15, 13);
-  // Serial2.begin(19200);
-   // Serial2.setTimeout(5000);
-   //Serial1.begin(19200, SERIAL_8N1, 32, 33);
-
+LedsAnim = engine->leds->anim("decharge");
+LedsAnim->setParam(0, TimeOut);
+// LedsAnim->setParam(11,41);
+//
+// engine->leds->play(LedsAnim);
 
 
 }
@@ -205,98 +77,210 @@ void setup() {
 
 void loop()
 {
-  /******** Routine every 2 sec ***********/
-   if ((millis() - currentTime) > 2000)
+
+  if (mode == 0) // Gauge Level
+  {
+  /******** Routine every 5 sec ***********/
+   if ((millis() - currentTime) > InterruptTime * 1000)
    {
-      currentTime=millis();
-
-     if(!(engine->power->charge))
-     {
-       if (previousChargeState)
-        {
-          engine->audio->stop();
-          engine->audio->loop(false);
-          engine->audio->play( "/decharge1.mp3" );
-          engine->leds->stop();
-          previousChargeState=false;
-          LedsAnim = engine->leds->anim("decharge");
-          engine->leds->play(LedsAnim);
-        }
-
-       if(engine->power->SOC < 40 )
+       if(!(engine->power->charge))
        {
-         LedsAnim->setParam(3, 255);
-         LedsAnim->setParam(4, 165);
-         LedsAnim->setParam(5, 0);
-         LedsAnim->setParam(7, 200);
-         LedsAnim->setParam(8, 0);
-         LedsAnim->setParam(9, 0);
+
+       /* Set Colors */
+         if((engine->power->SOC < 40 ) && (engine->power->SOC >= 10 ))
+         {
+           LedsAnim = engine->leds->anim("decharge"); // Set anim to decharge
+           LedsAnim->setParam(0, TimeOut); // Set TimeOut
+           LedsAnim->setParam(3, Color2[0]);
+           LedsAnim->setParam(4, Color2[1]);
+           LedsAnim->setParam(5, Color2[2]);
+           LedsAnim->setParam(7, Color3[0]);
+           LedsAnim->setParam(8, Color3[1]);
+           LedsAnim->setParam(9, Color3[2]);
+         }
+         else if(engine->power->SOC < 10 )
+         {
+           if (sinusOn)
+           {
+           LedsAnim = engine->leds->anim("sinus");
+           LedsAnim->setParam(3, Color3[0]);
+           LedsAnim->setParam(4, Color3[1]);
+           LedsAnim->setParam(5, Color3[2]);
+           LedsAnim->setParam(6, 0);
+           sinusOn=false;
+           }
+           else
+           {
+             LedsAnim = engine->leds->anim("chaser");
+             LedsAnim->setParam(3, Color3[0]);
+             LedsAnim->setParam(4, Color3[1]);
+             LedsAnim->setParam(5, Color3[2]);
+             LedsAnim->setParam(6, 0);
+             sinusOn = true;
+           }
+         } else // SOC > 40
+         {
+           LedsAnim = engine->leds->anim("decharge"); // Set anim to decharge
+           LedsAnim->setParam(0, TimeOut); // Set TimeOut
+           LedsAnim->setParam(3, Color1[0]);
+           LedsAnim->setParam(4, Color1[1]);
+           LedsAnim->setParam(5, Color1[2]);
+           LedsAnim->setParam(7, Color2[0]);
+           LedsAnim->setParam(8, Color2[1]);
+           LedsAnim->setParam(9, Color2[2]);
+
+         }
        }
-       else if(engine->power->SOC < 10 )
+       else
        {
-         // engine->leds->stop();
-         // LedsAnim = engine->leds->anim("sinus");
-         // engine->leds->play(LedsAnim);
-         // engine->audio->play( "/decharge1.mp3" );
-       } else
-       {
-         LedsAnim->setParam(3, 0);
-         LedsAnim->setParam(4, 100);
-         LedsAnim->setParam(5, 0);
-         LedsAnim->setParam(7, 255);
-         LedsAnim->setParam(8, 165);
-         LedsAnim->setParam(9, 0);
+         LedsAnim = engine->leds->anim("charge"); // Set anim to charge
+         LedsAnim->setParam(0, TimeOut); // Set TimeOut
+
+       /* Set Colors */
+          if((engine->power->SOC < 40 )&& (engine->power->SOC >= 10 ))
+          {
+            LedsAnim->setParam(3, Color2[0]);
+            LedsAnim->setParam(4, Color2[1]);
+            LedsAnim->setParam(5, Color2[2]);
+            LedsAnim->setParam(7, Color3[0]);
+            LedsAnim->setParam(8, Color3[1]);
+            LedsAnim->setParam(9, Color3[2]);
+          }
+          else if(engine->power->SOC < 10 )
+          {
+            LedsAnim->setParam(3, Color3[0]);
+            LedsAnim->setParam(4, Color3[1]);
+            LedsAnim->setParam(5, Color3[2]);
+            LedsAnim->setParam(7, 0);
+            LedsAnim->setParam(8, 0);
+            LedsAnim->setParam(9, 0);
+          } else
+          {
+            LedsAnim->setParam(3, Color1[0]);
+            LedsAnim->setParam(4, Color1[1]);
+            LedsAnim->setParam(5, Color1[2]);
+            LedsAnim->setParam(7, Color2[0]);
+            LedsAnim->setParam(8, Color2[1]);
+            LedsAnim->setParam(9, Color2[2]);
+          }
        }
-       LedsAnim->setParam(11, engine->power->SOC);
 
-     }
-     else
+
+       LedsAnim->setParam(11, engine->power->SOC); // Set State Of Charge
+       engine->leds->play(LedsAnim); // Play anim
+     currentTime=millis();
+   }
+ } else if(mode == 1) // Power 1
+ {
+   if (millis() - currentTime > InterruptTime * 1000)
+   {
+     LedsAnim = engine->leds->anim("current"); // Set anim to current
+     LedsAnim->setParam(0, TimeOut); // Set TimeOut
+     LedsAnim->setParam(1, Color1[0]);
+     LedsAnim->setParam(2, Color1[1]);
+     LedsAnim->setParam(3, Color1[2]);
+     LedsAnim->setParam(5, Color2[0]);
+     LedsAnim->setParam(6, Color2[1]);
+     LedsAnim->setParam(7, Color2[2]);
+     PowerValue = engine->power->power() ;
+     LedsAnim->setParam(9, PowerValue/3000); // Set State Of Charge
+     engine->leds->play(LedsAnim); // Play anim
+     currentTime=millis();
+
+   }
+   if ((millis() - loopTime) > 200)
+   {
+     PowerValue = engine->power->power() ;
+     LedsAnim->setParam(9, PowerValue/3000); // Set Power Value
+   loopTime=millis();
+   }
+ }
+ else if(mode ==2) // Power 2
+ {
+   if (millis() - currentTime > InterruptTime * 1000)
      {
-       if (!previousChargeState )
-        {
-          engine->audio->loop(true);
-          engine->audio->play( "/robot.mp3" );
-          previousChargeState=true;
-          LedsAnim = engine->leds->anim("charge");
-          LedsAnim->setParam(11, engine->power->SOC);
-          engine->leds->play(LedsAnim);
-        }
+       LedsAnim = engine->leds->anim("current2"); // Set anim to current2
+       LedsAnim->setParam(0, TimeOut); // Set TimeOut
+       LedsAnim->setParam(1, Color1[0]);
+       LedsAnim->setParam(2, Color1[1]);
+       LedsAnim->setParam(3, Color1[2]);
+       LedsAnim->setParam(5, Color2[0]);
+       LedsAnim->setParam(6, Color2[1]);
+       LedsAnim->setParam(7, Color2[2]);
+       PowerValue = engine->power->power() ;
+       LedsAnim->setParam(9, PowerValue/3000); // Set State Of Charge
+       engine->leds->play(LedsAnim); // Play anim
+       currentTime=millis();
 
-        if(engine->power->SOC < 40 )
-        {
-          LedsAnim->setParam(3, 255);
-          LedsAnim->setParam(4, 165);
-          LedsAnim->setParam(5, 0);
-          LedsAnim->setParam(7, 200);
-          LedsAnim->setParam(8, 0);
-          LedsAnim->setParam(9, 0);
-        }
-        else if(engine->power->SOC < 10 )
-        {
-          LedsAnim->setParam(3, 200);
-          LedsAnim->setParam(4, 0);
-          LedsAnim->setParam(5, 0);
-          LedsAnim->setParam(7, 0);
-          LedsAnim->setParam(8, 0);
-          LedsAnim->setParam(9, 0);
-        } else
-        {
-          LedsAnim->setParam(3, 0);
-          LedsAnim->setParam(4, 100);
-          LedsAnim->setParam(5, 0);
-          LedsAnim->setParam(7, 255);
-          LedsAnim->setParam(8, 165);
-          LedsAnim->setParam(9, 0);
-        }
-        LedsAnim->setParam(11, engine->power->SOC);
+   }
+ if ((millis() - loopTime) > 200)
+   {
+     PowerValue = engine->power->power() ;
+     LedsAnim->setParam(9, PowerValue/3000); // Set Power Value
+   loopTime=millis();
+   }
+ } else if(mode ==3) // Double visu
+ {
+   if (millis() - currentTime > InterruptTime * 1000)
+     {
+       LedsAnim = engine->leds->anim("double"); // Set anim to current2
+       LedsAnim->setParam(0, TimeOut); // Set TimeOut
+       LedsAnim->setParam(1, Color1[0]);
+       LedsAnim->setParam(2, Color1[1]);
+       LedsAnim->setParam(3, Color1[2]);
+       LedsAnim->setParam(5, Color2[0]);
+       LedsAnim->setParam(6, Color2[1]);
+       LedsAnim->setParam(7, Color2[2]);
+       LedsAnim->setParam(10, engine->power->SOC);
 
-     }
-  }
+       PowerValue = engine->power->power() ;
+       LedsAnim->setParam(9, PowerValue/3000); // Set State Of Charge
+       engine->leds->play(LedsAnim); // Play anim
+       currentTime=millis();
+
+   }
+ if ((millis() - loopTime) > 200)
+   {
+     PowerValue = engine->power->power() ;
+     LedsAnim->setParam(9, PowerValue/3000); // Set Power Value
+   loopTime=millis();
+   }
+ } else if (mode == 4)
+ {
+   if (millis() - currentTime > InterruptTime * 1000)
+   {
+       if (sinusOn)
+       {
+       LedsAnim = engine->leds->anim("sinus");
+       LedsAnim->setParam(3, Color3[0]);
+       LedsAnim->setParam(4, Color3[1]);
+       LedsAnim->setParam(5, Color3[2]);
+       LedsAnim->setParam(6, 0);
+       sinusOn=false;
+       }
+       else
+       {
+         LedsAnim = engine->leds->anim("chaser");
+         LedsAnim->setParam(3, Color3[0]);
+         LedsAnim->setParam(4, Color3[1]);
+         LedsAnim->setParam(5, Color3[2]);
+         LedsAnim->setParam(6, 0);
+         sinusOn = true;
+       }
+
+      engine->leds->play(LedsAnim); // Play anim
+     currentTime=millis();
+    }
+ }
+
 
 
    if (engine->stm32->clicked()) {
+     mode ++;
+     if (mode == 5) mode = 0;
 
-     LedsAnim->setParam(11, 20);
+
+
 
    }
    else if (engine->stm32->dblclicked())
